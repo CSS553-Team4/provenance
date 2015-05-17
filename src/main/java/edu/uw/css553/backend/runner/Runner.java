0837512b7fc0e4432fd1f680be8ba267e844c8ff
Christina Burnett <cburn17@uw.edu>
2015-05-17 16:05:56 -0700

    draft

--- src/main/java/edu/uw/css553/backend/runner/Runner.java
+++ src/main/java/edu/uw/css553/backend/runner/Runner.java
@@ -1,10 +1,14 @@
 package edu.uw.css553.backend.runner;
 
+import java.util.Calendar;
+
+import edu.uw.css553.backend.entities.Action;
 import edu.uw.css553.backend.entities.Workflow;
+import edu.uw.css553.backend.logger.FileLogger;
 
 /**
  * @author Christina Burnett
- * @version "1.0, 05/16/15"
+ * @version "1.0, 05/17/15"
  *
  * The Runner class implements the RunnerInterface.
  * This class performs the actions defined in a given workflow, logging each
@@ -14,21 +18,43 @@ public class Runner implements RunnerInterface {
     @Override
     public int executeWorkflow (Workflow workflow) {
         int i = 1;
-        int j = 1;
-        if (workflow.actions.length > 0) {
+        int success = 1;
+        Action action;
+        Object outputObject = new Object();
+        int actionCount = workflow.getActions().size();
+        if (actionCount > 0) {
+            Calendar calendar = Calendar.getInstance();
+            String filename = "theFileName.txt"; //TODO determine pattern for filename construction
+            Object inputObject = new Object(); //TODO this must be the input data for the Workflow
             // instantiate the logger
+            FileLogger logger = new FileLogger(filename);
             // initialize log for this workflow
+            //TODO Logger expects unique number for Workflow, but Workflow does not supply one
+            logger.initWorkflowLog( 1 , calendar.getTime());
+            while (i <= actionCount && success == 1) {
+                action = workflow.getActions().get(i);
+                // initialize action
+                //TODO Logger expects Action name, but Action does not supply one
+                logger.initAction(action.toString(), calendar.getTime());
+                try {
+                    // execute action
+                    outputObject = action.execute(inputObject);
+                }
+                catch (RuntimeException ex) {
+                    System.out.println("Runtime exception in step " + i + " of " + workflow.getName());
+                    System.out.println(ex.toString());
+                    success = 0;
+                }
+                // terminate action
+                logger.terminateAction(outputObject.toString(), calendar.getTime());
+                // prepare for next action by passing output into next step's input
+                inputObject = outputObject;
+                i++;
+            }
+            // terminate workflow
+            logger.terminateWorkflowLog(outputObject.toString(), calendar.getTime());
         }
-        while (i < workflow.actions.length && j != 0)
-        {
-            // initialize action
-            // execute action
-            j = workflow.actions[i].length(); // fake out pending Action interface
-            // terminate action
-            i++;
-        }
-        // terminate workflow
-        return j;
+        return success;
     }
 }
 
