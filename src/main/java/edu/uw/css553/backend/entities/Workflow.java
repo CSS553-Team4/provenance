d68719c43ae67041442144069755beb89fe3fa6e
LandoUWB <deltasquad451@gmail.com>
2015-05-18 13:59:26 -0700

    Renamed the workflow related classes

--- src/main/java/edu/uw/css553/backend/entities/Workflow.java
+++ src/main/java/edu/uw/css553/backend/entities/Workflow.java
@@ -1,43 +1,112 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package edu.uw.css553.backend.entities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * WorkflowInterface responsible for providing a layout, though not an implemen-
- * tion of the workflow object itself
+ * An implementation of the workflow interface
+ *
  * @author Zulqurnain Hussain
  */
-public interface Workflow {
+public class Workflow implements WorkflowInterface {
+
+    public class NoSuchActionException extends Exception {
+
+        public NoSuchActionException(String message) {
+            super(message);
+        }
+    }
+    private String name;
+    private ArrayList<Action> actions;
+    private boolean isFileLoc = false;
+    private String worflowInput;
+    public boolean isWorkflowOpen = false;
+    public int workflowId;
+
+    public Workflow(String name) {
+        this.name = name;
+        this.actions = new ArrayList<Action>();
+    }
+
     /**
      * Add an action to the workflow
+     *
      * @param action : an action to be added to the workflow
      */
-    public void addAction(Action action);
-    /**
-     * Remove a specified action from the workflow, given an action
-     * @param action : an action object to be removed from the workflow actions
-     */
-    public void removeAction(Action action) throws WorkflowInstance.NoSuchActionException;
+    @Override
+    public void addAction(Action action) {
+        this.actions.add(action);
+    }
+
     /**
      * Get a list of the actions that are part of the workflow
+     *
      * @return a list of all the action elements in workflow
      */
-    public List<Action> getActions();
+    @Override
+    public List<Action> getActions() {
+        return this.actions;
+    }
+
     /**
-     * Set a name for the workflow
-     * @param name : the name for the workflow
+     * Remove a specified action from the workflow, given an action
+     * @exception NoSuchActionException : The action doesnt exist in the workflow
+     * @param action : an action object to be removed from the workflow actions
      */
-    public void setName(String name);
+    @Override
+    public void removeAction(Action action) throws NoSuchActionException{
+        if(!this.actions.contains(action)){
+            throw new NoSuchActionException("The specified action doesn't exists");
+        }
+        else{
+            this.actions.remove(action);
+        }
+    }
+
     /**
      * Get the name of the workflow
+     *
      * @return String name of the workflow object
      */
-    public String getName();
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
     /**
-     * Allows for the initial specification of the input that would go into
-     * a workflow
+     * Set a name for the workflow
+     *
+     * @param name : the name for the workflow
+     */
+    @Override
+    public void setName(String name) {
+        if (!name.equals("")) {
+            this.name = name;
+        }
+    }
+
+    /**
+     * Allows for the initial specification of the input that would go into a
+     * workflow
+     *
      * @param input : input can be user input or a specified file
      * @param isFile : flag for determining if the input is a file or not
      */
-    public void setWorkflowInput(String input, boolean isFile);
+    @Override
+    public void setWorkflowInput(String input, boolean isFile) {
+        this.name = input;
+        this.isFileLoc = isFile;
+    }
+
+    public boolean isInputFile() {
+        return this.isFileLoc;
+    }
+
+    public String getWorkflowInput() {
+        return this.worflowInput;
+    }
 }
