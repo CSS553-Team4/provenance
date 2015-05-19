45afcacda04db54e38f7a1b7d93d1dbf640a77e3
Miles Dowe <milesdowe@D-10-156-2-69.dhcp4.washington.edu>
2015-05-18 17:36:48 -0700

    Added general implementation of WorkflowManager object. Workflow object implements Serializable interface.

--- src/main/java/edu/uw/css553/backend/manager/WorkflowManager.java
+++ src/main/java/edu/uw/css553/backend/manager/WorkflowManager.java
@@ -1,8 +1,16 @@
 package edu.uw.css553.backend.manager;
 
-import edu.uw.css553.backend.entities.Workflow;
-
 import java.util.List;
+import java.util.ArrayList;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.File;
+import java.io.Serializable;
+
+import edu.uw.css553.backend.entities.Workflow;
 
 public interface WorkflowManager {
 
@@ -12,8 +20,13 @@ public interface WorkflowManager {
     public void saveWorkflow( Workflow workflow );
 
     /**
+     * Responsible for retrieving workflow from datastore
+     */
+    public Workflow openWorkflow( String directory );
+
+    /**
      * Responsible for retrieving all workflows from the data store
      */
-    public List<Workflow> listAvailableWorkflows();
+    public List<Workflow> listAvailableWorkflows( String directory );
 
 }
